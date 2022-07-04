e98ebaa99b0aca80245a58bb472a908174d24b91
cristina <cristinaaran89@gmail.com>
2022-07-04 11:15:53 +0200

    [archivo .env]

--- models/Electrics.model.ts
+++ models/Electrics.model.ts
@@ -1,4 +1,12 @@
 const { Schema, model } = require('mongoose');
+enum Consumptions{
+    A,"A+++" = 0.25,
+    B = 0.5,
+    C = 0.75,
+    D = 0.4,
+
+}
+
 const appilanceSchema = new Schema(
     {
         name: {
@@ -14,8 +22,10 @@ const appilanceSchema = new Schema(
             required: true,
         },
         consum: {
-            enum: ['']
+            enum: ['A+++', 'A++', 'A+', 'A', 'B', 'C', 'D']
         }
+        // en 2021 cambio a solamente A, B, C , D
+        // hasta 20221 era 'A+++', 'A++', 'A+', B, 'C', 'D'
     }, {
     timestamps: true
 }
