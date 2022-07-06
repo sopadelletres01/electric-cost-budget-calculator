6788dd8c501d86a19e4858723e4a3d23a213698d
macape01 <macape@fp.insjoaquimmir.cat>
2022-07-06 20:05:27 +0200

    a

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,37 @@
 const router = require('express').Router();
 
+const Price = require("../models/Price.model")
+const Appliance = require("../models/Appliance.model")
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/test", async (req, res, next) => {
+
+  //const price =  await Price.findById(id)
+  const prices = await Price.find({})
+  const appliances = await Appliance.find({})
+  const price = prices[0]
+  //const appliance = appliances[0]
+  const index = prices.indexOf(price)
+  let minutes = 20
+  let numberOfHours = Math.floor(minutes / 60)
+  console.log(numberOfHours)
+  if(numberOfHours < 1) {
+    let power = 0.00345 //MW
+    let hours = minutes / 60
+    let energy = power * hours
+    let pricee = (price.price * energy) / 1 //€ / MWh 
+    let total = pricee 
+    console.log("pow",power, "hours",hours, "energy",energy, "price",pricee ,"total",total)
+  }
+
+
+
+
+  res.render('test');
+})
 
 module.exports = router;
